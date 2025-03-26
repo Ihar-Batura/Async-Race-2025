a33fb26d616120d187668024a97b2590c3f839ff
Ihar-Batura <A17331582@gmail.com>
2025-03-26 15:32:51 +0300

    feat: add function getGarageData

---
+++ src/api/garage/get_data.ts
@@ -0,0 +1,21 @@
+import { ICarsData } from '../../types/types';
+
+async function getGarageData(url: string) {
+  const baseUrl: string = 'http://127.0.0.1:3000';
+  const fullUrl: string = `${baseUrl}${url}`;
+
+  try {
+    const response: Response = await fetch(fullUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data: ICarsData[] = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error in function getGarageData');
+  }
+}
+
+export default getGarageData;
