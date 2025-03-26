b43640a8cd1de5c5a440557dad1af3f2ef22b590
Ihar-Batura <A17331582@gmail.com>
2025-03-26 13:27:57 +0300

    feat: add function createGaragePage

---
+++ src/pages/garage.ts
@@ -0,0 +1,26 @@
+import createElement from '../create/create_element';
+import createGarageControlsContainer from '../components/garage_controls_container';
+import createPageNavigation from '../components/navigation';
+
+function createGaragePage(): HTMLElement {
+  const garage: HTMLElement = createElement({
+    tag: 'div',
+    classes: ['wrapper', 'main-wrapper'],
+  });
+
+  const control: HTMLElement = createGarageControlsContainer();
+  const carsAmount: HTMLElement = createElement({
+    tag: 'h3',
+    text: '',
+    classes: ['garage-cars-amount'],
+  });
+
+  const navigation: HTMLElement = createPageNavigation();
+  const box = createElement({ tag: 'div', classes: ['garage-box'] });
+
+  garage.append(control, carsAmount, navigation, box);
+
+  return garage;
+}
+
+export default createGaragePage;
