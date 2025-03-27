46719f3d0b88e5328653665ffedf6e1b8dc2e8d5
Ihar-Batura <A17331582@gmail.com>
2025-03-27 16:51:24 +0300

    feat: add function stateBtnsPrevAndNext

---
+++ src/functional/button/state_btns_prev_and_next.ts
@@ -0,0 +1,38 @@
+import whatPageNumber from '../info/what_page_number';
+import isPageWinners from '../info/is_page_winners';
+import howMatchThereCarsOrWinners from '../info/how_much_there_cars_or_winners';
+import changeDisabledElementById from '../element/change_element_disable_by_id';
+
+function stateBtnsPrevAndNext(): void {
+  const page: string = whatPageNumber();
+  const pageWinners: boolean = isPageWinners();
+  let pages: boolean | string;
+  let pagesAmount: number;
+
+  if (pageWinners) {
+    const pagesWinners: string = howMatchThereCarsOrWinners('winners-cars-amount');
+    const pagesWinnersAmount: number = Math.ceil(+pagesWinners / 10);
+    pages = pageWinners;
+    pagesAmount = pagesWinnersAmount;
+  } else {
+    const pagesGarage: string = howMatchThereCarsOrWinners('garage-cars-amount');
+    const pagesGarageAmount: number = Math.ceil(+pagesGarage / 7);
+    pages = pagesGarage;
+    pagesAmount = pagesGarageAmount;
+  }
+
+  if (page === '1') {
+    changeDisabledElementById('btn-prev', true);
+  }
+  if (page !== '1') {
+    changeDisabledElementById('btn-prev', false);
+  }
+  if (+page < pagesAmount) {
+    changeDisabledElementById('btn-next', false);
+  }
+  if (+page === pagesAmount) {
+    changeDisabledElementById('btn-next', true);
+  }
+}
+
+export default stateBtnsPrevAndNext;
