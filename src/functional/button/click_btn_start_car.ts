02140ea568d2d35fb995ae82b5143c375b282071
Ihar-Batura <A17331582@gmail.com>
2025-03-27 16:49:55 +0300

    feat: add function clickBtnStartOneCar

---
+++ src/functional/button/click_btn_start_car.ts
@@ -0,0 +1,35 @@
+import startCarEngine from '../../api/engine/start_engine';
+import changeDisabledChildElement from '../element/change_disabled_child_element';
+
+function clickBtnStartOneCar(): void {
+  const startBtnsList: NodeListOf<Element> = document.querySelectorAll('.car-item-btn-start');
+
+  if (startBtnsList.length > 0) {
+    startBtnsList.forEach((btn) => {
+      btn.addEventListener('click', function (event: Event) {
+        if (!event || !event.target) return;
+
+        const target: EventTarget = event.target;
+
+        if (!(target instanceof HTMLButtonElement)) {
+          return;
+        }
+
+        const parent = target.offsetParent;
+
+        if (!(parent instanceof HTMLDivElement)) {
+          return;
+        }
+
+        const parentId: string = parent.id;
+
+        startCarEngine(parentId);
+
+        btn.setAttribute('disabled', 'true');
+        changeDisabledChildElement(parentId, 'car-item-btn-return', false);
+      });
+    });
+  }
+}
+
+export default clickBtnStartOneCar;
