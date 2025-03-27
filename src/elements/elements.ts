d855e0e9e2f00ede45b0406564b18178ef9006f6
Ihar-Batura <A17331582@gmail.com>
2025-03-27 16:44:21 +0300

    chore: delete some files

--- src/elements/elements.ts
+++
@@ -1,9 +0,0 @@
-import createElement from '../create/create_element';
-
-export const carsAmountElement = createElement({
-  tag: 'h3',
-  text: 'Cars in the garage (0)',
-  classes: ['garage-cars-amount'],
-});
-
-export const garageCarsBox = createElement({ tag: 'div', classes: ['garage-box'] });
