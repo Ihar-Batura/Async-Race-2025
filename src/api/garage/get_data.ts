96a6b0b63220868900dbdd5c792816c693f029c8
Ihar-Batura <A17331582@gmail.com>
2025-03-27 13:00:11 +0300

    refactor: add type to function getGarageData

--- src/api/garage/get_data.ts
+++ src/api/garage/get_data.ts
@@ -1,6 +1,6 @@
-import { ICarsData } from '../../types/types';
+import { ICarData } from '../../types/types';
 
-async function getGarageData(url: string) {
+async function getGarageData(url: string): Promise<ICarData[] | undefined> {
   const baseUrl: string = 'http://127.0.0.1:3000';
   const fullUrl: string = `${baseUrl}${url}`;
 
@@ -11,10 +11,10 @@ async function getGarageData(url: string) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    const data: ICarsData[] = await response.json();
+    const data: ICarData[] = await response.json();
     return data;
   } catch (error) {
-    console.log('Error in function getGarageData');
+    console.log('Error in function getGarageData', error);
   }
 }
 
