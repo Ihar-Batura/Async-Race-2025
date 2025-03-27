3fc64960a076186301f5d66577f1649d750c5a31
Ihar-Batura <A17331582@gmail.com>
2025-03-27 16:49:20 +0300

    feat: add function clickBtnReturnCar

---
+++ src/functional/button/click_btn_return_car.ts
@@ -0,0 +1,32 @@
+import changeDisabledChildElement from '../element/change_disabled_child_element';
+
+function clickBtnReturnCar(): void {
+  const returnBtnsList: NodeListOf<Element> = document.querySelectorAll('.car-item-btn-return');
+
+  if (returnBtnsList.length > 0) {
+    returnBtnsList.forEach((btn) => {
+      btn.addEventListener('click', function (event: Event) {
+        if (!event || !event.target) return;
+
+        const target: EventTarget = event.target;
+
+        if (!(target instanceof HTMLButtonElement)) {
+          return;
+        }
+
+        const parent = target.offsetParent;
+
+        if (!(parent instanceof HTMLDivElement)) {
+          return;
+        }
+
+        const parentId: string = parent.id;
+
+        btn.setAttribute('disabled', 'true');
+        changeDisabledChildElement(parentId, 'car-item-btn-start', false);
+      });
+    });
+  }
+}
+
+export default clickBtnReturnCar;
